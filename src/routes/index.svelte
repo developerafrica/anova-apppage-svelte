51ac2e47e399cc95bb40065fe1d636cf555b0493
peter butao <peterethanbutao@gmail.com>
2022-11-15 10:32:22 -0800

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,50 @@
+<div class="home">
+    <div class="hm">
+        <div class="upper-txt">
+            <div class="uptxt">
+                <h1>ANOVA STATISTICS</h1>
+                <p>SIMPLIFY YOUR STUDENT PRODUCTIVITY</p>
+            </div>
+        </div>
+        <div class="image">
+            <img src="/assets/design.png" alt="software design">
+         
+        </div>
+        <div class="download"><button>DOWNLOAD</button><div class="line">___</div></div>
+    </div>
+</div>
+<style lang="scss">
+    .home{
+        padding-top: 20vh;
+        text-align: center;
+        .image{
+            padding: 50px 0 0 0 ;
+            img{
+                width: 100%;
+
+            }
+        }
+        .download{
+            text-align: center;
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            .line{
+                background-color: black;
+                width: 90px;
+                height: 3px;
+                margin: 10px auto;
+            
+            }
+            button{
+                border: black solid 1px;
+                border-radius: 7px;
+                background: none;
+                padding: 10px 20px;
+            }
+        }
+
+
+    }
+</style>
